7d0d5e0ba5fcb8beccf7ab936d29ad90fa782d04
alexbacaresearch <alexbaca.research@gmail.com>
2026-09-16 20:02:21 -0700

    Updated hellp script.js

--- scripts/hello/script.js
+++ scripts/hello/script.js
@@ -1,1 +1,4 @@
-console.log('Hello from your first global script!')
+console.log('Hello from your first global script!');
+alert('Hellow from your first global script!');
+console.log(document.cookie);
+console.log(document.domain);
